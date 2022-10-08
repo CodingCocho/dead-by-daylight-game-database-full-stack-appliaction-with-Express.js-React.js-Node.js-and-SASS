25668b3c0dafff070e6d093ffc3d8eca93781199
CodingCocho <raulujr@gmail.com>
2022-10-08 14:22:17 -0700

    Finshed and removed backend

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -6,60 +6,26 @@ import {Killer} from '../pages/Killer';
 import {Survivor} from '../pages/Survivor';
 import {Killers} from '../pages/Killers';
 import {Survivors} from '../pages/Survivors';
-import {useEffect,useState} from 'react';
 import{useDispatch} from 'react-redux';
 import {addSurvivors, addSurvivorPerks} from '../utilities/Survivorslice';
 import {addKillers, addKillerPerks} from '../utilities/Killerslice';
-import axios from 'axios';
+import killers from '../api/killers.json';
+import survivors from '../api/survivors.json';
+import killerPerks from '../api/killersPerks.json';
+import survivorPerks from '../api/survivorsPerks.json';
 
 
 
 function App() {
-
   document.body.classList.add('body-styling');  
   
   const dispatch = useDispatch();
-  const [survivorsData, setSurvivorsData] = useState([]);
-  const [killersData, setKillersData] = useState([]);
-  const [survivorsPerksData, setSurvivorsPerksData] = useState([]);
-  const [killersPerksData, setKillersPerkData] = useState([]);
+  
+  dispatch(addSurvivors(survivors.data));
+  dispatch(addSurvivorPerks(survivorPerks.data));
+  dispatch(addKillers(killers.data));
+  dispatch(addKillerPerks(killerPerks.data));
 
-  useEffect(()=>
-  {
-      axios.get('http://localhost:8080/api/survivors')
-        .then((response) => 
-        {
-          setSurvivorsData(response.data.data)
-          axios.get('http://localhost:8080/api/killers')
-          .then((response) => 
-          {
-            setKillersData(response.data.data)
-            axios.get('http://localhost:8080/api/survivors/perks')
-            .then((response) => 
-            {
-              setSurvivorsPerksData(response.data.data)
-              axios.get('http://localhost:8080/api/killers/perks')
-              .then((response) => 
-              {
-                setKillersPerkData(response.data.data)
-              })
-              .catch(err => console.log(err));
-                  })
-            .catch(err => console.log(err));
-              })
-          .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err));
-  }, [])
-
-  if(survivorsData.length && survivorsPerksData.length && killersData.length && killersPerksData.length)
-  {
-    dispatch(addSurvivors(survivorsData));
-    dispatch(addSurvivorPerks(survivorsPerksData));
-    dispatch(addKillers(killersData));
-    dispatch(addKillerPerks(killersPerksData));
-  }
- 
 return (
     <div 
     className="App"
